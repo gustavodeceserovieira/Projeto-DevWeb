2cb41b40b3b90b13fb600d91cbc78254d49328e1
Gustavo <gustavodeceserovieira@gmail.com>
2025-09-06 22:00:48 -0300

    Versão final para deploy

--- server.js
+++ server.js
@@ -50,5 +50,4 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
-
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
